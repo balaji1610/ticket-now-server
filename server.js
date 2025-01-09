675614ebc8cd77b421c1d46f5a00cc7a06fe048d
Balaji <81224567+balaji1610@users.noreply.github.com>
2025-01-09 16:03:59 +0530

    Add Fronted URL

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000", //frontend
+    origin: "https://ticket-now-user-login-ui-branch.onrender.com", //frontend
     credentials: true,
   })
 );
