8101c9c0a05c9b7d3cc1b3ac40f3cae07827b401
Balaji <81224567+balaji1610@users.noreply.github.com>
2025-01-11 22:55:43 +0530

    modified authlogin

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,7 +60,7 @@ exports.authLogin = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
     });
-    res.status(200).json({ message: "Login successful", token });
+    res.status(200).json({ message: "Login successful", result: user });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
